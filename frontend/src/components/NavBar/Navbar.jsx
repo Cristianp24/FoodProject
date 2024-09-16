096c938fe90898782913f04d1020b897a7b1d3cf
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2024-09-16 13:52:26 -0300

    Add Conditional Render Navbar Button Logout/Login

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Navbar.css'; // Asegúrate de crear este archivo
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate(); // Cambiado useHistory por useNavigate
+
+  // Verificar si el usuario está autenticado
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Elimina el token del localStorage
+    setIsAuthenticated(false); // Actualiza el estado para reflejar que ya no está autenticado
+    navigate('/login'); // Redirige al login
   };
 
   return (
@@ -16,7 +33,13 @@ const Navbar = () => {
       <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <a href="/home" className="nav-link">Home</a>
         <a href="/create-meal" className="nav-link">Calculate Meal</a>
-        <a href="/login" className="nav-link">Login</a>
+        {isAuthenticated ? (
+          <button onClick={handleLogout} className="nav-link logout-button">
+            Logout
+          </button>
+        ) : (
+          <a href="/login" className="nav-link">Login</a>
+        )}
       </div>
       <div className="navbar-toggle" onClick={toggleMenu}>
         <span className="hamburger"></span>
